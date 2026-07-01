f18b8d075f7f4cf757c25b1013041fa94e414b56
JayyyXp <49588855+JayyyXp@users.noreply.github.com>
2026-07-01 21:58:38 +0800

    Add: convert-bst-to-greater-tree - submission-4

---
+++ Data Structures & Algorithms/convert-bst-to-greater-tree/submission-4.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        vals = []
+        q = deque([root])
+        while q:
+            node = q.popleft()      
+            if not node:
+                continue
+            vals.append(node.val)
+            q.append(node.left)
+            q.append(node.right)
+
+        vals.sort()
+        vtoi = {v: i for i,v in enumerate(vals)}
+        prefix = []
+        acc = 0
+        for v in vals:
+            acc += v
+            prefix.append(acc)
+
+        q = deque([root])
+        while q:
+            node = q.popleft()      
+            if not node:
+                continue
+            node.val += acc - prefix[vtoi[node.val]] 
+            vals.append(node.val)
+            q.append(node.left)
+            q.append(node.right)
+
+        return root
