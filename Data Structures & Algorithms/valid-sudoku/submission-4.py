584cc6c1bf65f060fb92954f8f683b8d5ff45633
JayyyXp <49588855+JayyyXp@users.noreply.github.com>
2026-07-23 23:33:47 +0800

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        ROWS = 9
+        COLS = 9
+        def isValid(counter):
+            counter.pop(".", None)
+            return any(c > 1 for n, c in counter.items() )
+
+        for r in range(ROWS):
+            counter = Counter(board[r])
+            if isValid(counter):
+                return False
+
+        for c in range(COLS):
+            counter = Counter([board[r][c] for r in range(ROWS)])
+            if isValid(counter):
+                return False
+
+        for r in range(0, ROWS, 3):
+            for c in range(0, COLS, 3):
+                helper = []
+                for rr in range(r, r + 3):
+                    for cc in range(c, c + 3):
+                        helper.append(board[rr][cc])
+                counter = Counter(helper)
+                if isValid(counter):
+                    return False
+                
+        return True 
